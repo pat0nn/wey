82e14349c2066c738123facc4d8e4e8ac04e2e34
trong <phambatrong.pbt@gmail.com>
2024-01-24 17:46:05 +0700

    commit

--- wey_frontend/src/components/Trends.vue
+++ wey_frontend/src/components/Trends.vue
@@ -1,89 +1,43 @@
 <template>
-  <div class="p-4 bg-white border border-gray-200 rounded-lg">
-    <h3 class="mb-6 text-xl">Trends</h3>
-    <div class="space-y-4">
-      <div class="flex items-center justify-between">
-        <div class="flex items-center space-x-2">
-          <p class="text-xs">
-            <strong>#codewithstein</strong><br /><span class="text-gray-500"
-              >180 posts</span
-            >
-          </p>
-        </div>
-        <a
-          href="#"
-          class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg"
-          >Explore</a
-        >
-      </div>
-      <div class="flex items-center justify-between">
-        <div class="flex items-center space-x-2">
-          <p class="text-xs">
-            <strong>#codewithstein</strong><br /><span class="text-gray-500"
-              >180 posts</span
-            >
-          </p>
-        </div>
-        <a
-          href="#"
-          class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg"
-          >Explore</a
-        >
-      </div>
-      <div class="flex items-center justify-between">
-        <div class="flex items-center space-x-2">
-          <p class="text-xs">
-            <strong>#codewithstein</strong><br /><span class="text-gray-500"
-              >180 posts</span
-            >
-          </p>
-        </div>
-        <a
-          href="#"
-          class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg"
-          >Explore</a
-        >
-      </div>
-      <div class="flex items-center justify-between">
-        <div class="flex items-center space-x-2">
-          <p class="text-xs">
-            <strong>#codewithstein</strong><br /><span class="text-gray-500"
-              >180 posts</span
-            >
-          </p>
-        </div>
-        <a
-          href="#"
-          class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg"
-          >Explore</a
-        >
-      </div>
-      <div class="flex items-center justify-between">
-        <div class="flex items-center space-x-2">
-          <p class="text-xs">
-            <strong>#codewithstein</strong><br /><span class="text-gray-500"
-              >180 posts</span
-            >
-          </p>
-        </div>
-        <a
-          href="#"
-          class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg"
-          >Explore</a
-        >
-      </div>
-    </div>
-  </div>
-</template>
+<div class="p-4 bg-white border border-gray-200 rounded-lg">
+        <h3 class="mb-6 text-xl">Trends</h3>
 
-<!-- <script>
-import PeopleYouMayKnow from "../components/PeopleYouMayKnow.vue";
-import Trends from "../components/Trends.vue";
-export default {
-  name: "SearchView",
-  components: {
-    PeopleYouMayKnow,
-    Trends,
-  },
-};
-</script> -->
+        <div class="space-y-4">
+            <div class="flex items-center justify-between">
+                <p class="text-xs">
+                    <strong>#codewithstein</strong><br>
+                    <span class="text-gray-500">180 posts</span>
+                </p>
+
+                <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</a>
+            </div>
+
+            <div class="flex items-center justify-between">
+                <p class="text-xs">
+                    <strong>#codewithstein</strong><br>
+                    <span class="text-gray-500">180 posts</span>
+                </p>
+
+                <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</a>
+            </div>
+
+            <div class="flex items-center justify-between">
+                <p class="text-xs">
+                    <strong>#codewithstein</strong><br>
+                    <span class="text-gray-500">180 posts</span>
+                </p>
+
+                <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</a>
+            </div>
+
+            <div class="flex items-center justify-between">
+                <p class="text-xs">
+                    <strong>#codewithstein</strong><br>
+                    <span class="text-gray-500">180 posts</span>
+                </p>
+
+                <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</a>
+            </div>
+        </div>
+    </div>
+</template>
