9c8ec83e46d0d1696789e2d237edf30c7d5225ea
trong <phambatrong.pbt@gmail.com>
2024-03-05 11:57:18 +0700

    update

---
+++ wey_frontend/src/views/SearchView.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
+    <div class="main-left col-span-3 space-y-4">
+      <div class="bg-white border border-gray-200 rounded-lg">
+        <form v-on:submit.prevent="submitForm" class="p-4 flex space-x-4">
+          <input
+            v-model="query"
+            type="search"
+            class="p-4 w-full bg-gray-100 rounded-lg"
+            placeholder="What are you looking for?"
+          />
+
+          <button
+            class="inline-block py-4 px-6 bg-purple-600 text-white rounded-lg"
+          >
+            Post
+          </button>
+        </form>
+      </div>
+
+      <div
+        class="p-4 bg-white border border-gray-200 rounded-lg grid grid-cols-4 gap-4"
+        v-if="users.length"
+      >
+        <div
+          class="p-4 text-center bg-gray-100 rounded-lg"
+          v-for="user in users"
+          :key="user.id"
+        >
+          <img
+            src="https://i.pravatar.cc/300?img=70"
+            class="mb-6 rounded-full"
+          />
+
+          <p>
+            <strong>
+              <RouterLink :to="{ name: 'profile', params: { id: user.id } }">
+                {{ user.name }}
+              </RouterLink>
+            </strong>
+          </p>
+
+          <div class="mt-6 flex space-x-8 justify-around">
+            <p class="text-xs text-gray-500">{{ user.friends_count }}</p>
+            <p class="text-xs text-gray-500">120 posts</p>
+          </div>
+        </div>
+      </div>
+
+      <div
+        class="p-4 bg-white border border-gray-200 rounded-lg"
+        v-for="post in posts"
+        v-bind:key="post.id"
+      >
+        <FeedItem v-bind:post="post" />
+      </div>
+    </div>
+
+    <div class="main-right col-span-1 space-y-4">
+      <PeopleYouMayKnow />
+
+      <Trends />
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import PeopleYouMayKnow from "../components/PeopleYouMayKnow.vue";
+import Trends from "../components/Trends.vue";
+import { RouterLink } from "vue-router";
+import FeedItem from "@/components/FeedItem.vue";
+
+export default {
+  name: "SearchView",
+  components: {
+    PeopleYouMayKnow,
+    Trends,
+    RouterLink,
+    FeedItem,
+  },
+
+  data() {
+    return {
+      query: "",
+      users: [],
+      posts: [],
+    };
+  },
+  methods: {
+    submitForm() {
+      console.log("submitForm", this.query);
+
+      // axios
+      //     .post('/api/search/',{
+      //         query: this.query
+      //     })
+      //     .then(response => {
+      //         console.log('response: ',response.data)
+      //     })
+      //     .catch(error => {
+      //         console.log('error', error)
+      //     })
+
+      axios
+        .post("/api/search/", {
+          query: this.query,
+        })
+        .then((response) => {
+          console.log("response:", response.data);
+
+          this.users = response.data.users;
+          this.posts = response.data.posts;
+        })
+        .catch((error) => {
+          console.log("error:", error);
+        });
+    },
+  },
+};
+</script>
---
+++ wey_frontend/src/views/test.vue
@@ -0,0 +1,142 @@
+<script>
+
+import axios from 'axios'
+import { useToastStore } from '../stores/toast'
+export default{
+    setup() {
+        const toastStore = useToastStore()
+
+        return {
+            toastStore
+        }
+    },
+
+    data() {
+        return {
+            form: {
+                email: '',
+                name: '',
+                password1: '',
+                password2: '',
+            },
+            errors: [],
+        }
+    },
+
+    methods: {
+        submitForm() {
+            this.errors = []
+
+            if (this.form.name=== '') {
+                this.errors.push('Your name is missing')
+            }
+
+            if (this.form.email=== '') {
+                this.errors.push('Your email is missing')
+            }
+
+            if (this.form.password1=== '') {
+                this.errors.push('Your password is missing')
+            }
+
+
+            if (this.form.password1 !== this.form.password2) {
+                this.errors.push('The password does not match')
+            }
+
+            if (this.error.length === 0) {
+                axios
+                    .post('/api/signup/', this.form)
+                    .then (response => {
+                        if (response.data.message === 'success') {
+                            this.toastStore.showToast(5000, 'The user is registered. Please log in', 'bg-emerald-500')
+
+                            this.form.email = ''
+                            this.form.name = ''
+                            this.form.password1 = ''
+                            this.form.password1 = ''
+                        } else {
+                            this.toastStore.showToast(5000, 'Sonething went wrong. Please try again', 'bg-red-300')
+                        }
+                    })
+                    .catch(error => {
+                        console.log('error', error)
+                    })
+            }
+        }
+    }
+} 
+</script>
+
+// adsefasfasfasf
+
+
+<script>
+import axios from 'axios'
+
+import { useToastStore } from '@/stores/toast'
+
+export default {
+    setup() {
+        const toastStore = useToastStore()
+
+        return {
+            toastStore
+        }
+    },
+
+    data() {
+        return {
+            form: {
+                email: '',
+                name: '',
+                password1: '',
+                password2: ''
+            },
+            errors: [],
+        }
+    },
+
+    methods: {
+        submitForm() {
+            this.errors = []
+
+            if (this.form.email === '') {
+                this.errors.push('Your e-mail is missing')
+            }
+
+            if (this.form.name === '') {
+                this.errors.push('Your name is missing')
+            }
+
+            if (this.form.password1 === '') {
+                this.errors.push('Your password is missing')
+            }
+
+            if (this.form.password1 !== this.form.password2) {
+                this.errors.push('The password does not match')
+            }
+
+            if (this.errors.length === 0) {
+                axios
+                    .post('/api/signup/', this.form)
+                    .then(response => {
+                        if (response.data.message === 'success') {
+                            this.toastStore.showToast(5000, 'The user is registered. Please log in', 'bg-emerald-500')
+
+                            this.form.email = ''
+                            this.form.name = ''
+                            this.form.password1 = ''
+                            this.form.password2 = ''
+                        } else {
+                            this.toastStore.showToast(5000, 'Something went wrong. Please try again', 'bg-red-300')
+                        }
+                    })
+                    .catch(error => {
+                        console.log('error', error)
+                    })
+            }
+        }
+    }
+}
+</script>
